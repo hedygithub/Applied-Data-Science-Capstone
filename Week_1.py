551ef4c7230d93fa2e3446420e59f0a53b7a9e7a
unknown <you@example.com>
2019-06-17 22:15:07 +0800

    adds a python script according to assignment of week 1

---
+++ Week_1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# This notebook will be mainly used for the project of Applied Data Science Capstone.
+import pandas as pd
+import numpy as np
+print ('Hello Capstone Project Course!')
+
