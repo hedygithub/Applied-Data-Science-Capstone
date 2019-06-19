f5ffc6600b28b3a8518ac265c61df5c49559e93b
hedygithub <di.he@foxmail.com>
2019-06-19 22:17:48 +0800

    Delete Week_1.py

--- Week_1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# This notebook will be mainly used for the project of Applied Data Science Capstone.
-import pandas as pd
-import numpy as np
-print ('Hello Capstone Project Course!')
-
